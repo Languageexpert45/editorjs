1517c224dd10fa928c552f202762b2fdbf34d78e
Alex <cocos_@mail.ru>
2022-10-14 12:37:39 +0300

    second

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
-import EditorJS from '@editorjs/editorjs';
-import ImageTool from '@editorjs/image';
-
+import EditorJS from "@editorjs/editorjs";
+import ImageTool from "@editorjs/image";
 
 const editor = new EditorJS({
   /**
    * Id of Element that should contain Editor instance
    */
-  holder: 'editorjs',
+  holder: "editorjs",
   /**
    * Available Tools list.
    * Pass Tool's class or Settings object for each Tool you want to use
@@ -16,10 +15,29 @@ const editor = new EditorJS({
       class: ImageTool,
       config: {
         endpoints: {
-          byFile: '', // Your backend file uploader endpoint
-          byUrl: '', // Your endpoint that provides uploading by Url
-        }
-      }
-    }
+          byFile: "http://127.0.0.1:8080/src/storeImages.js", // Your backend file uploader endpoint
+          byUrl: "http://127.0.0.1:8080/src/storeImages.js", // Your endpoint that provides uploading by Url
+        },
+        uploader: {
+          /**
+           * Upload file to the server and return an uploaded image data
+           * @param {File} file - file selected from the device or pasted by drag-n-drop
+           * @return {Promise.<{success, file: {./src}}>}
+           */
+          uploadByFile(file) {
+            // console.log(file);
+            return MyAjax.upload(file).then(() => {
+              return {
+                success: 1,
+                file: {
+                  url: "https://codex.so/upload/redactor_images/o_80beea670e49f04931ce9e3b2122ac70.jpg",
+                  // any other image data you want to store, such as width, height, color, extension, etc
+                },
+              };
+            });
+          },
+        },
+      },
+    },
   },
 });
